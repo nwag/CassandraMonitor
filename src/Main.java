30c34fded240afbacfc895e910b3f0e2c757d51b
Nico Wagner <nicolas.wagner7@gmx.net>
2016-06-15 16:41:30 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import java.time.Instant;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+
+/**
+ * 
+ * @author nicolas wagner
+ *
+ */
+public class Main{
+
+	private final static int SAMPLETIME_MS = 5000;
+
+	private static Logger logger = Logger.getLogger("CassandraStats");
+	private static FileHandler fh;
+
+	public static void main( String[] args ) {
+		try {
+			JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://134.60.64.93:7199/jmxrmi");
+			JMXConnector jmxc = JMXConnectorFactory.connect(url, null); 
+			MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
+
+			long unixTimestamp = Instant.now().getEpochSecond();
+			fh = new FileHandler("../CassandraMonitor/stats"+unixTimestamp+".log");  
+			logger.addHandler(fh);
+			SimpleFormatter formatter = new SimpleFormatter();  
+			fh.setFormatter(formatter);  
+
+
+			ObjectName attribute0 = new ObjectName("org.apache.cassandra.metrics:type=ColumnFamily,name=TotalDiskSpaceUsed");
+			ObjectName attribute1 = new ObjectName("org.apache.cassandra.metrics:type=ColumnFamily,name=ReadLatency");
+			ObjectName attribute2 = new ObjectName("org.apache.cassandra.metrics:type=ColumnFamily,name=WriteLatency");
+			ObjectName attribute3 = new ObjectName("org.apache.cassandra.metrics:type=Client,name=connectedNativeClients");
+			ObjectName attribute4 = new ObjectName("org.apache.cassandra.metrics:type=Client,name=connectedThriftClients");
+
+			String attribute0Unit = "byte";
+			String attribute1Unit =  mbsc.getAttribute(attribute1, "DurationUnit").toString();
+			String attribute2Unit =  mbsc.getAttribute(attribute2, "DurationUnit").toString();
+
+			while(true){
+				String attribute0Status =  mbsc.getAttribute(attribute0, "Value").toString();
+				String attribute1Status =  mbsc.getAttribute(attribute1, "Mean").toString();
+				String attribute2Status =  mbsc.getAttribute(attribute2, "Mean").toString();
+				String attribute3Status =  mbsc.getAttribute(attribute3, "Value").toString();
+				String attribute4Status =  mbsc.getAttribute(attribute4, "Value").toString();
+
+				logger.info("TotalDiskSpaceUsed: "+attribute0Status+" "+attribute0Unit+"\nReadLatency: "+attribute1Status+" "+attribute1Unit+"\nWriteLatency: "+ attribute2Status+" "+attribute2Unit+"\nconnectedNativeClients: "+attribute3Status+"\nconnectedThriftClients: "+attribute4Status);
+				Thread.sleep(SAMPLETIME_MS);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	} 
+}
